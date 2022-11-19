279c266941c790e74bc0fd40290eed052c4d2601
richzago <rich.czago@gmail.com>
2022-11-18 21:14:51 -0300

    feat: main of python

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from management.product_handler import get_product_by_id, get_products_by_type, menu_report, add_product
+from management.tab_handler import calculate_tab
+from menu import products
+products2 = [{}]
 if __name__ == "__main__":
-    # Seus prints de teste aqui
     ...
